775cba9975c4b27407234e274926d3fdfcd47998
Bojan Golic <bokigolic32@gmail.com>
2022-06-11 02:20:13 -0400

    Add Navbar component, impore router

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navbar(){
+  return(
+    <>
+     <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo'>
+
+            TRVL
+            <i class='fab fa-typo3' />
+          </Link>
+       
+      </div>
+    </nav>
+  </>
+  )
+
+}
+
+export default Navbar
